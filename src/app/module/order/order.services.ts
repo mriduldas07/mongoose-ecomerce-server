9dc60786df0855d412ce10097391b30e38bf4d97
mriduldas07 <mriduldas0325@gmail.com>
2024-07-05 17:02:44 +0600

    updated order quantity

--- src/app/module/order/order.services.ts
+++ src/app/module/order/order.services.ts
@@ -1,7 +1,44 @@
+import ApiError from "../../errors/ApiError";
+import { Product } from "../product/product.model";
 import { IOrder } from "./order.interface";
 import { Order } from "./order.model";
 
 const createOrder = async (payload: IOrder): Promise<IOrder> => {
+  const { quantity, productId, ...others } = payload;
+  const product = await Product.findById(productId).lean();
+  console.log(product);
+
+  if (product && product.inventory.quantity < quantity) {
+    throw new ApiError(400, "Insufficient quantity available in inventory");
+  }
+  if (product && product.inventory.quantity >= quantity) {
+    const newQuantity = Number(product.inventory.quantity - quantity);
+
+    if (newQuantity === 0) {
+      const updatedData = {
+        ...product,
+        inventory: {
+          quantity: newQuantity,
+          inStock: false,
+        },
+      };
+      await Product.findOneAndUpdate({ _id: productId }, updatedData, {
+        new: true,
+      });
+    } else {
+      const updatedData = {
+        ...product,
+        inventory: {
+          quantity: newQuantity,
+          inStock: product.inventory.inStock,
+        },
+      };
+      await Product.findOneAndUpdate({ _id: productId }, updatedData, {
+        new: true,
+      });
+    }
+  }
+
   const result = (await Order.create(payload)).populate("productId");
   return result;
 };
